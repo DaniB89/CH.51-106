e23e061876585213b56644f85f89cae80c0d00d4
DaniB89 <danielle.black@sdgku.edu>
2024-09-28 15:03:39 -0700

    sessions3

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+function saveTask(){
+    //get values        
+    const title = $("#txtTitle").val();
+    const desc = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selSatus").val();
+    const budget = $("#numBudget").val();
+    console.log(title,desc,color,date,status,budget)
+    //build an object
+    let taskToSave = new Task(title,desc,color,date,status,budget);
+    console.log(taskToSave);
+
+    //save to server (post)
+
+    //display the task (get)
+}
+function init(){
+    console.log("init")
+    $("#btnSave").click(saveTask)
+}
+
+window.onload = init;
